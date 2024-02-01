56456cfc0e0e49f3a1e9d1b16cbf0d938395be34
jreiber9 <128739362+jreiber9@users.noreply.github.com>
2024-01-31 20:08:28 -0500

    readded postgre

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -157,6 +157,5 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
